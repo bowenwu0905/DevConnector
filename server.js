d21a588bfdcf90a40a5f9e49ad9a4e55d0c523d0
bowenwu0826 <68851709+bowenwu0826@users.noreply.github.com>
2022-08-16 16:03:10 -0700

    finished draft

--- server.js
+++ server.js
@@ -4,17 +4,17 @@ const path = require("path");
 
 const app = express();
 
-//Connect Database
+// Connect Database
 connectDB();
 
-app.use(express.json({ extended: false }));
-app.get("/", (req, res) => res.send("API running"));
+// Init Middleware
+app.use(express.json());
 
 // Define Routes
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
-app.use("/api/posts", require("./routes/api/posts"));
 app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
 
 // Serve static assets in production
 if (process.env.NODE_ENV === "production") {
@@ -26,6 +26,6 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const PORT = process.env.PORT || 5010;
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
